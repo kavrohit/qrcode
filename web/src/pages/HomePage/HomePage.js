05339a09c8e701f5864c6208778234f7edb205c2
rohitAniLabs <rohit@anilabsinc.com>
2023-04-26 23:16:47 +0530

    minor fixes

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,27 @@
+import QRCode from 'qrcode'
+
+import QRCodeComponent from 'src/components/QRCode/QrCode'
+
+
+const HomePage = () => {
+  const generateQRCode = async (url) => {
+    try {
+      const code = await QRCode.toDataURL(url)
+      return code
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  return (
+    <>
+      <div>
+        <h1>QR Code Generator</h1>
+      </div>
+      <div>
+        <QRCodeComponent generate={generateQRCode} />
+      </div>
+    </>
+  )
+}
+
+export default HomePage
